fb1e790fbe5002c711ed30273c3e0449649e5a70
mai <fujisawamai21@gmail.com>
2021-10-27 20:11:39 +0900

    リファくた

--- operations/swap.c
+++ operations/swap.c
@@ -1,50 +1,41 @@
 #include "../push_swap.h"
 
-void    swap_a(t_stuck *a)
+void    swap_top(t_stuck *stuck)
 {
     t_node *top_to_second;
     t_node *second_to_top;
     t_node *third;
     t_node *bottom;
 
-    top_to_second = a->top;
+    top_to_second = stuck->top;
     second_to_top = top_to_second->next;
     third = second_to_top->next;
 
-    second_to_top->prev = a->bottom;
+    second_to_top->prev = stuck->bottom;
     second_to_top->next = top_to_second;
 
     top_to_second->prev = second_to_top;
     top_to_second->next = third;
 
-    bottom = a->bottom;
+    third->prev = top_to_second;
+
+    bottom = stuck->bottom;
     bottom->next = second_to_top;
 
-    a->top = second_to_top;
+    stuck->top = second_to_top;
+}
+
+//todo:Do nothing if thereis only one or no elements
+void    swap_a(t_stuck *a)
+{
+    swap_top(a);
     ft_putstr_fd("sa\n", 0);
 }
 
+//todo:Do nothing if thereis only one or no elements
 void    swap_b(t_stuck *b)
 {
-    t_node *top_to_second;
-    t_node *second_to_top;
-    t_node *third;
-    t_node *bottom;
-
-    top_to_second = b->top;
-    second_to_top = top_to_second->next;
-    third = second_to_top->next;
-
-    second_to_top->prev = b->bottom;
-    second_to_top->next = top_to_second;
-
-    top_to_second->prev = second_to_top;
-    top_to_second->next = third;
-
-    bottom = b->bottom;
-    bottom->next = second_to_top;
-
-    b->top = second_to_top;
+    swap_top(b);
     ft_putstr_fd("sb\n", 0);
 }
 
