ce4e66126c687db0e28fbfa35bb89aaef2af200e
mai <fujisawamai21@gmail.com>
2021-12-16 11:58:24 +0900

    引数が何も渡されない場合は何も表示せずプログラムを終了する

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mai <mai@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 22:00:11 by mai               #+#    #+#             */
-/*   Updated: 2021/12/16 11:20:53 by mai              ###   ########.fr       */
+/*   Updated: 2021/12/16 11:55:50 by mai              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	main(int argc, char **argv)
 	stack_a = (t_stack *)malloc(sizeof(t_stack));
 	stack_b = (t_stack *)malloc(sizeof(t_stack));
 	
+	if (argc == 1)
+		return (0);
 	create_nodes(argc - 1, argv, stack_a);
 	quicksort(stack_a, stack_b);
 	return (0);
