2cc3692bef7730a747234f2996d51d366b9ec962
mai <fujisawamai21@gmail.com>
2021-12-16 18:37:00 +0900

    int範囲外が入ってる場合は"Error"と表示してプログラムを終了する

--- main.c
+++ main.c
@@ -6,29 +6,41 @@
 /*   By: mai <mai@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 22:00:11 by mai               #+#    #+#             */
-/*   Updated: 2021/12/16 13:12:33 by mai              ###   ########.fr       */
+/*   Updated: 2021/12/16 18:34:06 by mai              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-bool is_notnum(int argc, char **argv)
+bool is_not_intnum(int argc, char **argv)
 {
 	int i;
 	int j;
 	int cnt;
+	long long val;
+	int	sign;
 
 	i = 1;
 	cnt = argc - 1;
 	while (cnt > 0)
 	{
+		val = 0;
 		j = 0;
+		sign = 1;
 		if (argv[i][j] == '-')
+		{
 			j = 1;
+			sign = -1;
+		}
 		while (argv[i][j])
 		{
 			if (!(ft_isdigit(argv[i][j])))
 				return (true);
+			if (sign == 1 && ((long long)10 * val + (argv[i][j] - '0')) > 2147483647)
+				return (true);
+			if (sign == -1 && ((long long)10 * val + (argv[i][j] - '0')) > 2147483648)
+				return (true);
+			val = 10 * val + (argv[i][j] - '0');
 			j++;
 		}
 		cnt--;
@@ -45,12 +57,11 @@ int	main(int argc, char **argv)
 	
 	if (argc == 1)
 		return (0);
-	if (is_notnum(argc, argv))
+	if (is_not_intnum(argc, argv))
 	{
 		ft_putstr_fd("Error\n", 0);
 		return (0);
 	}
-	return (0);
 	stack_a = (t_stack *)malloc(sizeof(t_stack));
 	stack_b = (t_stack *)malloc(sizeof(t_stack));
 	create_nodes(argc - 1, argv, stack_a);
